86d4e16dbd771d20ee5cba7e3a8d8fad2590bf55
AntFlopGame <73836816+AntFlopGame@users.noreply.github.com>
2023-02-24 13:40:10 +0200

    create landpage

--- client/src/components/home/footer/Footer.tsx
+++ client/src/components/home/footer/Footer.tsx
@@ -1,6 +1,4 @@
-
-
-import { Box, Container, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography, useTheme } from "@mui/material";
+import { Box, List, ListItem, ListItemText, Toolbar, Typography, useTheme } from "@mui/material";
 import logo  from '../../../../images/logo.png'
 
 
@@ -69,32 +67,35 @@ const Footer = () => {
     const theme = useTheme();
     
     return (
-        <Box component="footer" sx={{ bgcolor: theme.palette.primary.main, width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
-            <Box component="div" sx={{ maxWidth: "1300px", width: "inherit", alignItems: "center"}}>
-                <Toolbar>
-                    <Box>
-                        <Box component="img" src={ logo.src } alt="logo" style={{ width: "160px" }} />
+        <Box component="footer" sx={{ bgcolor: theme.palette.primary.main, width: "100%", display: "flex", flexDirection: "column", alignItems: "center", pt: "20px"}}>
+            <Box component="div" sx={{ maxWidth: "1300px", width: "inherit"}}>
+                <Toolbar sx={{ display: "flex",  alignItems: "center"}}>
+                    <Box component="div" sx={{ mr: "auto"}}>
+                        <Box component="img" src={ logo.src } alt="logo" style={{ width: "120pt" }} />
                         <Typography sx={{ color: theme.palette.secondary.main }}>© 2023, A-contract. All Rights Reserved.</Typography>
                     </Box>
-                    {navigation.map((value, index) => (
-                        <Box component="div" key={ index }>
-                            <List>
-                                <ListItem>
-                                    <ListItemText primary={
-                                        <Typography sx={{ color: value.color }}>{ value.title }</Typography>
-                                    }>
-                                    </ListItemText>
-                                </ListItem>
-                                {value.items.map((valueItem, indexItem) => (
-                                    <ListItem component="a" href={ valueItem.href } key={ indexItem }>
+                    <Box component="div" sx={{ display: "flex"}}>
+                        {navigation.map((value, index) => (
+                            <Box component="div" key={ index }>
+                                <List>
+                                    <ListItem>
                                         <ListItemText primary={
-                                            <Typography sx={{ color: valueItem.color }}>{ valueItem.name }</Typography>
-                                        }></ListItemText>
+                                            <Typography sx={{ color: value.color }}>{ value.title }</Typography>
+                                        }>
+                                        </ListItemText>
                                     </ListItem>
-                                ))}
-                            </List>
-                        </Box>
-                    ))}
+                                    {value.items.map((valueItem, indexItem) => (
+                                        <ListItem component="a" href={ valueItem.href } key={ indexItem }>
+                                            <ListItemText primary={
+                                                <Typography sx={{ color: valueItem.color }}>{ valueItem.name }</Typography>
+                                            }></ListItemText>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </Box>
+                        ))}
+                    </Box>
+                    
                 </Toolbar>
             </Box>
             
