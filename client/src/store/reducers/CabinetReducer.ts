cb173947cac48ce05f0c008fd95ce184ea7c4545
anthony <antfloppy@gmail.com>
2023-03-14 16:27:45 +0200

    create cabinet

--- client/src/store/reducers/CabinetReducer.ts
+++ client/src/store/reducers/CabinetReducer.ts
@@ -1,38 +1,38 @@
 import {
-    CabinetState,
-    CabinetAction,
-    CabinetActionTypes,
+  CabinetState,
+  CabinetAction,
+  CabinetActionTypes,
 } from "../../types/cabinet";
 
 const initialState: CabinetState = {
-    tabs: [
-        {
-            id: 0,
-            name: "Contracts",
-        },
-        {
-            id: 1,
-            name: "Support",
-        },
-        {
-            id: 2,
-            name: "Setting",
-        },
-    ],
-    activeTabId: 0,
+  tabs: [
+    {
+      id: 0,
+      name: "Contracts",
+    },
+    {
+      id: 1,
+      name: "Support",
+    },
+    {
+      id: 2,
+      name: "Setting",
+    },
+  ],
+  activeTabId: 0,
 };
 
 export const CabinetReducer = (
-    state = initialState,
-    action: CabinetAction
+  state = initialState,
+  action: CabinetAction
 ): CabinetState => {
-    switch (action.type) {
-        case CabinetActionTypes.SET_ACTIVE_CABINET_TAB:
-            return {
-                ...state,
-                activeTabId: action.payload,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CabinetActionTypes.SET_ACTIVE_CABINET_TAB:
+      return {
+        ...state,
+        activeTabId: action.payload,
+      };
+    default:
+      return state;
+  }
 };
