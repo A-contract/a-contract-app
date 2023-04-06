968807604428a5858ff6834d1574ad50d27f5ed1
anthony <antfloppy@gmail.com>
2023-04-06 10:23:19 +0300

    create roles

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,47 @@
-import { Module, RequestMethod } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
+// import { JwtModule, JwtService } from '@nestjs/jwt';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './entities/user.entity';
+// import { Role } from './entities/role.entity';
+// import { UserRole } from './entities/user_roles';
+// import { AuthController } from './modules/auth/auth.controller';
+// import { AuthService } from './modules/auth/auth.service';
+// import { RoleService } from './modules/role/role.service';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root',
+//       password: '',
+//       database: 'nest_auth',
+//       entities: [User, Role, UserRole],
+//       synchronize: true,
+//     }),
+//     TypeOrmModule.forFeature([User, Role, UserRole]),
+
+//     JwtModule.register({
+//       global: true,
+//       secret: process.env.JWT_SECRET_KEY,
+//       signOptions: { expiresIn: '1d' },
+//     }),
+//   ],
+//   controllers: [AuthController],
+//   providers: [AuthService, RoleService, JwtService],
+// })
+// export class AppModule {}
+
+import { Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './modules/auth/auth.controller';
-import { User } from './entities/user.entity';
 import { AuthService } from './modules/auth/auth.service';
+import { User } from './entities/user.entity';
 import { Role } from './entities/role.entity';
 import { UserRole } from './entities/user_roles';
 import { RoleService } from './modules/role/role.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-//
 
 @Module({
   imports: [
@@ -24,16 +56,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       synchronize: true,
     }),
     TypeOrmModule.forFeature([User, Role, UserRole]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_KEY'),
-        signOptions: { expiresIn: '1d' },
-      }),
-      inject: [ConfigService],
+    JwtModule.register({
+      secret: 'abdulalhazred',
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, RoleService, JwtService],
+  providers: [AuthService, RoleService],
 })
 export class AppModule {}
